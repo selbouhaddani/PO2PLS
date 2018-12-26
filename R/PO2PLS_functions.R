ab0d63f483b10d1cde218f7ca55709da8b91a0ab
Said el Bouhaddani <s.el_bouhaddani@lumc.nl>
2018-12-26 10:48:04 +0100

    minor

--- R/PO2PLS_functions.R
+++ R/PO2PLS_functions.R
@@ -157,9 +157,8 @@ Lemma <- function(X, SigmaZ, invZtilde, Gamma, sig2E, sig2F, p, q, r, rx, ry){
   return(list(EZc = EZc, VarZc = VarZc))
 }
 
-
 #' @export
-E_step <- function(X, Y, params){
+E_step_slow <- function(X, Y, params){
   ## retrieve parameters
   W = params$W
   C = params$C
@@ -338,7 +337,205 @@ E_step <- function(X, Y, params){
     loglik = loglik,
     comp_log = comp_log
   )
+
 }
+
+
+#' @export
+E_step <- function(X, Y, params){
+  ## retrieve parameters
+  W = params$W
+  C = params$C
+  Wo = params$Wo
+  Co = params$Co
+  B = params$B
+  SigT = params$SigT
+  SigTo = (ssq(Wo)>0)*params$SigTo +0# + 1e-10*(ssq(Wo)==0)
+  SigUo = (ssq(Co)>0)*params$SigUo +0# + 1e-10*(ssq(Co)==0)
+  SigH = params$SigH
+  sig2E = params$sig2E
+  sig2F = params$sig2F
+  SigU = SigT%*%B^2 + SigH
+
+  ## define dimensions
+  N = nrow(X)
+  p = nrow(W)
+  q = nrow(C)
+  r = ncol(W)
+  rx = ncol(Wo)
+  ry = ncol(Co)
+
+  ## concatenate data
+  dataXY <- cbind(X,Y)
+
+  ## Gamma is the generalized loading matrix, with PO2PLS structure
+  Gamma = rbind(cbind(W, matrix(0,p,r), Wo, matrix(0,p,ry)),
+                cbind(matrix(0,q,r), C, matrix(0,q,rx), Co))
+  ## Gamma multiplied by inverse SigmaEF
+  GammaEF <- Gamma
+  GammaEF[1:p,c(1:r,2*r+1:rx)] <- 1/sig2E* GammaEF[1:p,c(1:r,2*r+1:rx)]
+  GammaEF[-(1:p),c(r+1:r,2*r+rx+1:ry)] <- 1/sig2F* GammaEF[-(1:p),c(r+1:r,2*r+rx+1:ry)]
+  GGef <- t(Gamma) %*% GammaEF
+
+  ## diagonal cov matrix of (E,F), hopefully NOT NEEDED
+  # SigmaEF = diag(rep(c(sig2E,sig2F),c(p,q)))
+  ## ALMOST diagonal cov matrix of (T,U,To,Uo)
+  SigmaZ = blockm(
+    blockm(
+      blockm(SigT, SigT%*%B, SigU),
+      matrix(0,2*r,rx), SigTo),
+    matrix(0,2*r+rx,ry), SigUo)
+
+  ## inverse middle term lemma
+  invZtilde <- MASS::ginv(MASS::ginv(SigmaZ) + GGef)
+
+  ## Calculate conditional expectations with efficient lemma
+  # print(all.equal(invS,invS_old))
+  tmp <- Lemma(dataXY, SigmaZ, invZtilde, Gamma, sig2E, sig2F,p,q,r,rx,ry)
+  # print(all.equal(Lemma(cbind(X,Y), SigmaZ, invS, Gamma, sig2E, sig2F,p,q,r,rx,ry), Lemma_old(cbind(X,Y), SigmaZ, invS, Gamma)))
+
+  ## Define Szz as expected crossprod of Z
+  VarZc = tmp$VarZc
+  EZc = tmp$EZc
+  Szz = VarZc + crossprod(EZc)/N
+
+  ## For compatibility
+  # invEF_Gamma <- rbind(cbind(W, matrix(0,p,r), Wo, matrix(0,p,ry))/sig2E,
+  #                      cbind(matrix(0,q,r), C, matrix(0,q,rx), Co)/sig2F)
+  # inv2EF_Gamma <- rbind(cbind(W, matrix(0,p,r), Wo, matrix(0,p,ry))/(sig2E^2),
+  #                       cbind(matrix(0,q,r), C, matrix(0,q,rx), Co)/(sig2F^2))
+  # invZtilde <- MASS::ginv(MASS::ginv(SigmaZ) +
+  #                      t(Gamma) %*% rbind(cbind(W, matrix(0,p,r), Wo, matrix(0,p,ry))/sig2E,
+  #                                         cbind(matrix(0,q,r), C, matrix(0,q,rx), Co)/sig2F))
+  #
+  # invS_Gamma <- invEF_Gamma - invEF_Gamma %*% invZtilde %*% crossprod(invEF_Gamma,Gamma)
+
+  ## inverse in middle term in lemma
+  # invZtilde <- MASS::ginv(MASS::ginv(SigmaZ) +
+  #                      t(Gamma) %*% rbind(cbind(W, matrix(0,p,r), Wo, matrix(0,p,ry))/sig2E,
+  #                                         cbind(matrix(0,q,r), C, matrix(0,q,rx), Co)/sig2F))
+
+  # ## Calculate cond mean of E,F
+  # # invS_covEF <- diag(1,p+q) - invEF_Gamma %*% invZtilde %*% t(Gamma)
+  # # covEF = rbind(diag(sig2E,p), diag(0,q,p))
+  # # mu_EF_old = dataXY - dataXY %*% invEF_Gamma %*% invZtilde %*% t(Gamma)
+  # mu_EF = dataXY
+  # mu_EF <- mu_EF - (dataXY %*% (GammaEF %*% invZtilde)) %*% t(Gamma)
+  #
+  # ## Calculate immediately expected crossprod of E,F
+  # # Ceeff_old = SigmaEF - t(SigmaEF) %*% invS_covEF + crossprod(mu_EF) / N
+  # # Ceeff = Gamma %*% MASS::ginv(MASS::ginv(SigmaZ) + t(Gamma)%*%invSEF%*%Gamma) %*% t(Gamma) +
+  # #   crossprod(mu_EF) / N
+  # # print(all.equal(mu_EF_old, mu_EF))
+  # # print(all.equal(Ceeff_old, Ceeff))
+  #
+  # ## Take trace of the matrix
+  # # Cee_old <- sum(diag(Ceeff_old[1:p,1:p]))/p
+  # Cee <- sum(diag(
+  #   crossprod(rbind(cbind(W, matrix(0,p,r), Wo, matrix(0,p,ry)),
+  #                   cbind(matrix(0,q,r), 0*C, matrix(0,q,rx), 0*Co)))%*%invZtilde
+  # ))/p + ssq(mu_EF[,1:p])/N/p
+  # # Cff_old <- sum(diag(Ceeff_old[-(1:p),-(1:p)]))/q
+  # Cff <- sum(diag(
+  #   crossprod(rbind(cbind(0*W, matrix(0,p,r), 0*Wo, matrix(0,p,ry)),
+  #                   cbind(matrix(0,q,r), C, matrix(0,q,rx), Co)))%*%invZtilde
+  # ))/q + ssq(mu_EF[,-(1:p)])/N/q
+  # # cat('Cee\n'); print(all.equal(Cee_old,Cee));
+  # # print(all.equal(Cff_old,Cff))
+  #
+  # # Cee <- sum(diag(
+  # #   crossprod(rbind(cbind(W, matrix(0,p,r), Wo, matrix(0,p,ry)),
+  # #                   cbind(matrix(0,q,r), 0*C, matrix(0,q,rx), 0*Co)))%*%invZtilde
+  # # ))/p + ssq(mu_EF[,1:p])/N/p
+  # # Cff <- sum(diag(
+  # #   crossprod(rbind(cbind(0*W, matrix(0,p,r), 0*Wo, matrix(0,p,ry)),
+  # #                   cbind(matrix(0,q,r), C, matrix(0,q,rx), Co)))%*%invZtilde
+  # # ))/q + ssq(mu_EF[,-(1:p)])/N/q
+  #
+  #
+  # #covE = rbind(diag(sig2E,p), diag(0,q,p))
+  # #mu_E = cbind(X,Y) %*% invS %*% covE
+  # #Cee = sum(diag(diag(sig2E,p) - t(covE) %*% invS %*% covE + crossprod(mu_E) / N))/p
+  #
+  # #covF = rbind(diag(0,p,q), diag(sig2F,q))
+  # #mu_F = cbind(X,Y) %*% invS %*% covF
+  # #Cff = sum(diag(diag(sig2F,q) - t(covF) %*% invS %*% covF + crossprod(mu_F) / N))/q
+  #
+  # covH = rbind(0*W, C%*%SigH)
+  # covHEF = rbind(0*W, C%*%SigH/sig2F)
+  # # invS_covH <- (covH/sig2F - invEF_Gamma %*% invZtilde %*% crossprod(invEF_Gamma,covH))
+  # # mu_H_old = dataXY %*% invS_covH
+  # # mu_H_old <- dataXY %*% invS %*% covH
+  # mu_H <- dataXY %*% covHEF
+  # mu_H <- mu_H - (dataXY %*% (GammaEF %*% invZtilde)) %*% (t(Gamma) %*% covHEF)
+  # # Chh_old = SigH - t(covH) %*% invS_covH + crossprod(mu_H) / N
+  # # Chh_old <- SigH - t(covH) %*% invS %*% covH + crossprod(mu_H) / N
+  # Chh <- SigH
+  # Chh <- Chh - t(covH) %*% covHEF
+  # Chh <- Chh + (t(covH) %*% GammaEF %*% invZtilde) %*% (t(Gamma) %*% covHEF)
+  # Chh <- Chh + crossprod(mu_H) / N
+  # # print(all.equal(mu_H_old, mu_H))
+  # # print(all.equal(Chh_old, Chh))
+  #
+  # ## diagonal cov matrix of (X,Y), hopefully NOT NEEDED
+  # # SigmaXY = Gamma %*% SigmaZ %*% t(Gamma) + SigmaEF
+  # ## INVERSE diagonal cov matrix of (E,F), hopefully NOT NEEDED
+  # # invSEF <- diag(1/diag(SigmaEF))
+  # ## INVERSE diagonal cov matrix of (X,Y), hopefully NOT NEEDED
+  # # invS <- invSEF - invSEF %*% Gamma %*% MASS::ginv(MASS::ginv(SigmaZ) + t(Gamma)%*%invSEF%*%Gamma) %*% t(Gamma) %*% invSEF
+  # # if(use_lemma == TRUE){MASS::ginv(t(0))}
+  # ## log of det SigmaXY, see matrix determinant lemma
+  # logdet <- log(det(diag(2*r+rx+ry) + GGef%*%SigmaZ))+p*log(sig2E)+q*log(sig2F)
+  # ## representation of SigmaXY %*% invS
+  # XYinvS <- ssq(cbind(X/sqrt(sig2E), Y/sqrt(sig2F)))
+  # XYinvS <- XYinvS - sum(diag(crossprod(dataXY %*% GammaEF) %*% invZtilde))
+  # ## Log likelihood
+  # loglik = N*(p+q)*log(2*pi) + N * logdet + XYinvS
+  # loglik = - loglik/2
+  # # print(all.equal(XYinvS, sum(diag(dataXY %*% invS %*% t(dataXY)))))
+  # # MASS::ginv(t(0))
+  #
+  # comp_log <- - N/2*(p+q)*log(2*pi)
+  # comp_log <- comp_log - N/2*(p*log(sig2E)+q*log(sig2F))
+  # comp_log <- comp_log - N/2*ssq(cbind(X/sqrt(sig2E), Y/sqrt(sig2F)))
+  # comp_log <- comp_log + N*sum(diag(crossprod(EZc,dataXY)%*%GammaEF))
+  # comp_log <- comp_log - N/2*sum(diag(GGef%*%Szz))
+  #
+  # list(
+  #   EZc = EZc,
+  #   Szz = Szz,
+  #   mu_T = matrix(EZc[,1:r],N,r),
+  #   mu_U = matrix(EZc[,r+1:r],N,r),
+  #   mu_To = matrix(EZc[,2*r+1:rx],N,rx),
+  #   mu_Uo = matrix(EZc[,2*r+rx+1:ry],N,ry),
+  #   Stt = matrix(Szz[1:r, 1:r],r,r),
+  #   Suu = matrix(Szz[r+1:r, r+1:r],r,r),
+  #   Stoto = matrix(Szz[2*r+1:rx, 2*r+1:rx],rx,rx),
+  #   Suouo = matrix(Szz[2*r+rx+1:ry, 2*r+rx+1:ry],ry,ry),
+  #   Sut = matrix(Szz[r+1:r, 1:r],r,r),
+  #   Stto = matrix(Szz[1:r, 2*r+1:rx],r,rx),
+  #   Suuo = matrix(Szz[r+1:r, 2*r+rx+1:ry],r,ry),
+  #   See = Cee,
+  #   Sff = Cff,
+  #   Shh = Chh,
+  #   loglik = loglik,
+  #   comp_log = comp_log
+  # )
+  return(E_step_test(dataXY, p, q, r, rx, ry, N,
+                     SigmaZ, GammaEF, invZtilde, Gamma, GGef, EZc, Szz,
+                     W, C, Wo, Co, SigT, SigH, sig2E, sig2F))
+}
+
+E_step_test <- function(dataXY, p, q, r, rx, ry, N,
+                        SigmaZ, GammaEF, invZtilde, Gamma, GGef, EZc, Szz,
+                        W, C, Wo, Co, SigT, SigH, sig2E, sig2F){
+
+  return(E_step_testC(dataXY, p, q, r, rx, ry, N,
+                      SigmaZ, GammaEF, invZtilde, Gamma, GGef, EZc, Szz,
+                      W, C, Wo, Co, SigT, SigH, sig2E, sig2F))
+
+}
+
 
 #' @export
 M_step <- function(E_fit, params, X, Y, orth_type = c("SVD","QR")){
@@ -443,6 +640,64 @@ PO2PLS <- function(X, Y, r, rx, ry, steps = 1e5, tol = 1e-6, init_param='o2m',
   class(outputt) <- "PO2PLS"
   return(outputt)
 }
+
+#' @export
+PO2PLS_slow <- function(X, Y, r, rx, ry, steps = 1e5, tol = 1e-6, init_param='o2m',
+                   orth_type = "SVD", random_restart = FALSE){
+  if(all(c("W","Wo","C","Co","B","SigT","SigTo","SigUo","SigH","sig2E","sig2F") %in% names(init_param))) {message('using old fit \n'); params <- init_param}
+  else {params <- generate_params(X, Y, r, rx, ry, type = init_param)}
+  logl = 0*0:steps
+  tic <- proc.time()
+  print(paste('started',date()))
+
+  i_rr <- 0
+  random_restart_original <- random_restart
+  random_restart <- TRUE
+  while(random_restart){
+
+    if(i_rr > 0) {
+      message("Log-likelihood: ", logl[i+1])
+      message(paste("random restart no",i_rr))
+    }
+    params_max <- params
+    for(i in 1:steps){
+      E_next = E_step_slow(X, Y, params)
+      params_next = M_step(E_next, params, X, Y, orth_type = orth_type)
+      params_next$B <- abs(params_next$B)
+
+      if(i == 1) logl[1] = E_next$logl
+      logl[i+1] = E_next$logl
+      if(i > 1 && abs(logl[i+1]-logl[i]) < tol) break
+      if(i %in% c(1e1, 1e2, 1e3, 5e3, 1e4, 4e4)) {
+        print(data.frame(row.names = 1, steps = i, time = unname(proc.time()-tic)[3], diff = logl[i+1]-logl[i], logl = logl[i+1]))
+      }
+      if(logl[i+1] > max(logl[1:i])) params_max <- params_next
+      params = params_next
+    }
+    if(!any(diff(logl[-1]) < -1e-10) | !random_restart_original) {
+      random_restart = FALSE
+      break
+    }
+    i_rr <- i_rr + 1
+    params <- jitter_params(params)
+    params[-(1:4)] <- generate_params(X, Y, r, rx, ry, type = 'r')[-(1:4)]
+  }
+  # params <- params_max
+  signB <- sign(diag(params$B))
+  params$B <- params$B %*% diag(signB,r)
+  params$C <- params$C %*% diag(signB,r)
+  ordSB <- order(diag(params$SigT %*% params$B), decreasing = TRUE)
+  params$W <- params$W[,ordSB]
+  params$C <- params$C[,ordSB]
+  message("Nr steps was ", i)
+  message("Negative increments: ", any(diff(logl[0:i+1]) < 0),
+          "; Last increment: ", signif(logl[i+1]-logl[i],4))
+  message("Log-likelihood: ", logl[i+1])
+  outputt <- list(params = params_next, logl = logl[0:i+1][-1])
+  class(outputt) <- "PO2PLS"
+  return(outputt)
+}
+
 
 #' @export
 plot_accur.PO2PLS <- function(fit){
